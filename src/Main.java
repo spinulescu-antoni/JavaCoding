68871dbd59ba01643988b7cd1deffbcda2cd554e
spinulescu.antoni <toni07mich@gmail.com>
2023-10-19 19:45:23 +0300

    Metoda plus enum operatii

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
 import Task1.Calculator;
+import Task1.Operatii;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String operatie = scanner.next();
 
         Calculator calculator = new Calculator(5 , 10);
         calculator.setTermenUnu(2);
         calculator.setTermenDoi(5);
 
-        System.out.println(calculator.adunare());
+        if (operatie.equals(Operatii.ADUNARE.toString())) {
+            System.out.println(calculator.adunare());
+        } else if (Operatii.SCADERE.toString().equals(operatie)){
+            System.out.println(calculator.scadere());
+        } else {
+            System.out.println("Valoarea introdusa invalida");
+        }
+
+//        System.out.println(calculator.adunare());
+//        System.out.println(calculator.scadere());
     }
 }
