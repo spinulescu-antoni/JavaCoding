12f7992839cc8bd1a3fb54e1be4820ab38b2c8ce
spinulescu.antoni <toni07mich@gmail.com>
2023-10-19 21:12:55 +0300

    Numereimpare

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Introduceti codul operatiei intr 1 sau 2");
         Scanner scanner = new Scanner(System.in);
-        String operatie = scanner.next();
+//        String operatie = scanner.next();
 
         Calculator calculator = new Calculator(5 , 10);
         calculator.setTermenUnu(2);
         calculator.setTermenDoi(5);
-
-        if (operatie.equals(Operatii.ADUNARE.toString())) {
-            System.out.println(calculator.adunare());
-        } else if (Operatii.SCADERE.toString().equals(operatie)){
-            System.out.println(calculator.scadere());
-        } else {
-            System.out.println("Valoarea introdusa invalida");
-        }
-
 //        System.out.println(calculator.adunare());
-//        System.out.println(calculator.scadere());
+//        System.out.println(calculator.
+
+//        if (operatie.equals(Operatii.ADUNARE.toString())) {
+//            System.out.println(calculator.adunare());
+//        } else if (Operatii.SCADERE.toString().equals(operatie)){
+//            System.out.println(calculator.scadere());
+//        } else {
+//            System.out.println("Valoarea introdusa invalida");
+//        }
+//        int cod = scanner.nextInt();
+//        if (Operatii.ADUNARE.getCod() == cod){
+//            System.out.println(calculator.adunare());
+//        } else if (Operatii.SCADERE.getCod() == cod) {
+//            System.out.println(calculator.scadere());
+//        } else {
+//            System.out.println("Valoarea nu este valida");
+//        }
+
+        System.out.println(calculator.numereImpare());
+
+
+
+
+
     }
+
 }
--- src/Task1/Operatii.java
+++ src/Task1/Operatii.java
@@ -1,7 +1,23 @@
 package Task1;
 
 public enum Operatii {
-    ADUNARE,
-    SCADERE;
+    ADUNARE(1, "adunare", "OperatiideAdunare"),
+    SCADERE(2,"scadere", "OperatiideScadere");
+
+    private final int cod;
+    private final String valoare;
+
+    private final String nume;
+
+    Operatii(int cod, String valoare, String nume){
+        this.cod = cod;
+        this.valoare = valoare;
+        this.nume = nume;
+
+    }
+
+    public int getCod(){
+        return cod;
+    }
 
 }
